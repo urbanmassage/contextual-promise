2a6c26e37f639876e6b5bdc1e5eeaed7603a7276
Louay Alakkad <louay@alakkad.me>
2016-02-01 11:25:14 +0000

    full implementation

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,60 @@
-class ContextualPromise<T> extends Promise<T> {
-  private context: any;
-  constructor(fn) {
-    super(fn);
+// Native promises are not subclassable unfortunately
+import {Promise} from 'es6-promise';
+
+class ContextualPromise<T, C> extends Promise<T> {
+  private context: C;
+  constructor(executor: (resolve: (value?: T | Thenable<T>) => void, reject: (reason?: any) => void) => void, context?: C) {
+    super(executor && executor.bind(context));
+    this.context = context;
   }
 
-  bind(context) {
-    this.context = context;
-    return this;
+  bind<C>(context: C) {
+    return new ContextualPromise<T, C>((resolve, reject) => {
+      this.then(resolve, reject);
+    }, context);
+  }
+
+  then<U>(onResolve?: (value: T) => U | Thenable<U>, onReject?: (error: any) => U | Thenable<U>): ContextualPromise<U, C>;
+  then<U>(onResolve?: (value: T) => U | Thenable<U>, onReject?: (error: any) => void): ContextualPromise<U, C>;
+
+  then<U>(onResolve?, onReject?): ContextualPromise<U, C> {
+    return new ContextualPromise((resolve, reject) => {
+      super
+        .then<T>(onResolve && onResolve.bind(this.context), onReject && onReject.bind(this.context))
+        .then(resolve, reject);
+    }, this.context);
+  }
+
+  catch<U>(onReject?: (error: any) => U | Thenable<U>): ContextualPromise<U, C> {
+    return this.then(null, onReject);
+  }
+
+  static resolve(): ContextualPromise<void, void>;
+  static resolve<T>(v: T | Thenable<T>): ContextualPromise<T, void>;
+  static resolve<T, C>(v: T | Thenable<T>, ctx: C): ContextualPromise<T, C>;
+  static resolve<T, C>(v?: T | Thenable<T>, ctx?: C): ContextualPromise<T, C> {
+    return new ContextualPromise<T, C>(resolve => resolve(v), ctx);
+  }
+
+  static reject(): ContextualPromise<void, void>;
+  static reject<T>(v: T | Thenable<T>): ContextualPromise<T, void>;
+  static reject<T, C>(v: T | Thenable<T>, ctx: C): ContextualPromise<T, C>;
+  static reject<T, C>(v?: T | Thenable<T>, ctx?: C): ContextualPromise<T, C> {
+    return new ContextualPromise<T, C>((resolve, reject) => reject(v), ctx);
   }
   
-  then<T>(onResolve, onReject) {
-    return super.then<T>(onResolve.bind(this.context), onReject.bind(this.context));
+  static all<T>(values: Array<T | Thenable<T>>): ContextualPromise<T[], void>;
+  static all<T, C>(values: Array<T | Thenable<T>>, context: C): ContextualPromise<T[], C>;
+  static all<T, C>(values: Array<T | Thenable<T>>, context?: C): ContextualPromise<T[], C> {
+    return ContextualPromise.resolve(null, context)
+      .then<T[]>(() => Promise.all(values));
   }
-  
-  static resolve(): ContextualPromise<void>;
-  static resolve<T>(v: T | PromiseLike<T>): ContextualPromise<T>;
-  static resolve<T>(v?: T | PromiseLike<T>): ContextualPromise<T> {
-    return new ContextualPromise<T>(resolve => resolve(v));
+
+  static race<T>(values: Array<T | Thenable<T>>): ContextualPromise<T, void>;
+  static race<T, C>(values: Array<T | Thenable<T>>, context: C): ContextualPromise<T, C>;
+  static race<T, C>(values: Array<T | Thenable<T>>, context?: C): ContextualPromise<T, C> {
+    return ContextualPromise.resolve(null, context)
+      .then<T>(() => Promise.race(values));
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -2,17 +2,29 @@ import {expect} from 'chai';
 import ContextualPromise = require('../lib/index');
 
 describe('ContextualPromise', () => {
-  it('passes context', done => {
+  it('passes context', () => {
     const ctx = {test: 1};
-    ContextualPromise
+    return ContextualPromise
       .resolve(0)
       .bind(ctx)
       .then(function() {
-        let err;
-        try {
-          expect(this).to.deep.equal(ctx);
-        } catch (_err) {err = _err;}
-        done(err);
+        expect(this).to.deep.equal(ctx);
+      });
+  });
+  
+  it('passes context to executor', () => {
+    const ctx = {test: 1};
+    return new ContextualPromise(function (resolve, reject) {
+      expect(this).to.deep.equal(ctx);
+      resolve();
+    }, ctx);
+  });
+  
+  it('takes context as second argument', () => {
+    const ctx = {test: 1};
+    return new ContextualPromise(resolve => resolve(0), ctx)
+      .then(function() {
+        expect(this).to.deep.equal(ctx);
       });
   });
 });
